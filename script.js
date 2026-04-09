42caa3df9cbb98fb619ac206104513006a7e6f1a
Yurizcdev <yurizcrestani@gmail.com>
2026-04-09 20:50:37 -0300

    Add click event to remove list items

--- script.js
+++ script.js
@@ -4,6 +4,10 @@ function adicionar() {
     let li = document.createElement("li");
     li.innerText = produto;
 
+    li.onclick = function() {
+        li.remove();
+    };
+
     document.getElementById("lista").appendChild(li);
 
     document.getElementById("produto").value = "";
